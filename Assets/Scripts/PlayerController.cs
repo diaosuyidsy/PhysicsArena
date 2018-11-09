62bcda3e0740d512571afedcf418c4029be445ef
Suyi Diao <suyi.diao@vanderbilt.edu>
2018-11-09 16:36:59 -0500

    changed simple controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Debug.Log("Pressed A");
+        }
+        float HAxis = Input.GetAxis("Horizontal");
+        Debug.Log(HAxis);
+    }
+}

e1944be2060ad41529c35690455638b78fcd3c1d
TATammy <burningtammy@gmail.com>
2018-11-17 01:15:06 -0500

    Fixed car doesn't turn around when playerTag changes. Added non-working Team1 player script. Need fixing: when two playerTags collide with the trigger, car still moves.

--- Assets/Scripts/CarPath.cs
+++ Assets/Scripts/CarPath.cs
@@ -10,6 +10,7 @@ public class CarPath : MonoBehaviour
 	public float speed;
 
 	private int current = 5;
+	private int dir = 0;
 	
 	void OnTriggerStay(Collider other)
 	{
@@ -17,11 +18,23 @@ public class CarPath : MonoBehaviour
 		{
 			Debug.Log("Team 1 Wins");
 		}
+		if (current == 0)
+		{
+			Debug.Log("Team 2 Wins");
+		}
+	
+		if (other.CompareTag("Team1") && other.CompareTag("Team2"))
+		{
+			GetComponent<Rigidbody>().velocity = Vector3.zero;
+		}
 		
-		if (other.CompareTag("Team1") && current < target.Length)
+		else if (other.CompareTag("Team1") && current < target.Length)
 		{
 			if (transform.position != target[current].position)
-			{
+			{   if(dir == 2){
+					current = current + 1;
+				}
+				dir = 1;
 				Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
 				GetComponent<Rigidbody>().MovePosition(pos);
 			}
@@ -29,15 +42,14 @@ public class CarPath : MonoBehaviour
 
 		}
 
-		if (current == 0)
-		{
-			Debug.Log("Team 2 Wins");
-		}
-		
-		if (other.CompareTag("Team2") && current > 0)
+		else if (other.CompareTag("Team2") && current >= 0)
 		{
 			if (transform.position != target[current].position)
-			{
+			{   
+				if(dir == 1){
+					current = current - 1;
+				}
+				dir = 2;
 				Vector3 pos = Vector3.MoveTowards(transform.position, target[current].position, speed * Time.deltaTime);
 				GetComponent<Rigidbody>().MovePosition(pos);
 			}

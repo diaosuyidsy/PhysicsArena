1a8a277ac8c61d61a3d851a41d1e05e5e371bee6
TATammy <burningtammy@gmail.com>
2018-11-18 04:40:43 -0500

    Added a functioning dick.

---
+++ Assets/Scripts/rtEmit.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Obi;
+using UnityEngine;
+
+public class rtEmit : MonoBehaviour
+{
+	public GameObject WaterBall;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float Fire = Input.GetAxis("XboxRT");
+		Fire = Mathf.Approximately(Fire, 0f) || Mathf.Approximately(Fire, -1f) ? 0f : 1f;
+		WaterBall.GetComponent<ObiEmitter>().speed = Fire * 8f;
+	}
+}

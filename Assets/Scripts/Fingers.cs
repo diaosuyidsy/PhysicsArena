5b22bcd3a1b6295af6b040cc2da017701d570205
Suyi Diao <suyi.diao@vanderbilt.edu>
2018-11-17 23:16:57 -0500

    human both arms work

--- Assets/Scripts/Fingers.cs
+++ Assets/Scripts/Fingers.cs
@@ -7,7 +7,9 @@ public class Fingers : MonoBehaviour
 
     public float Force = 4000;
     Rigidbody rb;
-    bool taken = false;
+
+    [HideInInspector]
+    public bool taken = false;
 
     // Use this for initialization
     void Start()

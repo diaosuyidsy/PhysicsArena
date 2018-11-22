f634ae3ab5736f30bba61aa942d16d23f11d5365
diaosuyidsy <suyi.diao@vanderbilt.edu>
2018-11-22 00:28:34 -0500

    Fixed jump: no leg moving while jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,16 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [Tooltip("Enter 1 Digit 1-4")]
+    [Tooltip ("Enter 1 Digit 1-4")]
     public string PlayerControllerNumber;
     public float Thrust = 300f;
     public float JumpForce = 300f;
     public float RotationSpeed = 200f;
     public GameObject LegSwingReference;
     public GameObject Chest;
-    [Tooltip("Index 0 is Arm2, 1 is Arm, 2 is Hand")]
+    [Tooltip ("Index 0 is Arm2, 1 is Arm, 2 is Hand")]
     public GameObject[] LeftArms;
-    [Tooltip("Index 0 is Arm2, 1 is Arm, 2 is Hand")]
+    [Tooltip ("Index 0 is Arm2, 1 is Arm, 2 is Hand")]
     public GameObject[] RightArms;
 
     public GameObject TurnReference;
@@ -32,29 +32,29 @@ public class PlayerController : MonoBehaviour
 
     #endregion
 
-    private void Start()
+    private void Start ()
     {
-        _rb = GetComponent<Rigidbody>();
-        _distToGround = GetComponent<CapsuleCollider>().bounds.extents.y;
-        _chesthj = Chest.GetComponent<HingeJoint>();
-        _leftArm2hj = LeftArms[0].GetComponent<HingeJoint>();
-        _rightArm2hj = RightArms[0].GetComponent<HingeJoint>();
-        _leftArmhj = LeftArms[1].GetComponent<HingeJoint>();
-        _rightArmhj = RightArms[1].GetComponent<HingeJoint>();
-        _leftHandhj = LeftArms[2].GetComponent<HingeJoint>();
-        _rightHandhj = RightArms[2].GetComponent<HingeJoint>();
+        _rb = GetComponent<Rigidbody> ();
+        _distToGround = GetComponent<CapsuleCollider> ().bounds.extents.y;
+        _chesthj = Chest.GetComponent<HingeJoint> ();
+        _leftArm2hj = LeftArms[0].GetComponent<HingeJoint> ();
+        _rightArm2hj = RightArms[0].GetComponent<HingeJoint> ();
+        _leftArmhj = LeftArms[1].GetComponent<HingeJoint> ();
+        _rightArmhj = RightArms[1].GetComponent<HingeJoint> ();
+        _leftHandhj = LeftArms[2].GetComponent<HingeJoint> ();
+        _rightHandhj = RightArms[2].GetComponent<HingeJoint> ();
     }
 
     // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        CheckMovement();
-        CheckJump();
-        CheckArm();
+        CheckMovement ();
+        CheckJump ();
+        CheckArm ();
     }
 
 
-    private void CheckArm()
+    private void CheckArm ()
     {
         // For the left side
 #if UNITY_EDITOR_OSX
@@ -66,8 +66,8 @@ public class PlayerController : MonoBehaviour
         string LTStr = "Joy" + PlayerControllerNumber + "Axis9";
         string RTStr = "Joy" + PlayerControllerNumber + "Axis10";
 #endif
-        float LeftTrigger = Input.GetAxis(LTStr);
-        LeftTrigger = Mathf.Approximately(LeftTrigger, 0f) || Mathf.Approximately(LeftTrigger, -1f) ? 0f : 1f;
+        float LeftTrigger = Input.GetAxis (LTStr);
+        LeftTrigger = Mathf.Approximately (LeftTrigger, 0f) || Mathf.Approximately (LeftTrigger, -1f) ? 0f : 1f;
 
         // Arm2: Min -90 --> 89
         // Arm: Connected Mass Scale 1 --> 0
@@ -75,38 +75,38 @@ public class PlayerController : MonoBehaviour
         //      Limits: -90, -90 --> 81, 120
         // Hand: Limit Max: 90 --> 0
 
-        CheckArmHelper(LeftTrigger, _leftArm2hj, _leftArmhj, _leftHandhj, true);
-        CheckArmHelper(LeftTrigger, _rightArm2hj, _rightArmhj, _rightHandhj, false);
+        CheckArmHelper (LeftTrigger, _leftArm2hj, _leftArmhj, _leftHandhj, true);
+        CheckArmHelper (LeftTrigger, _rightArm2hj, _rightArmhj, _rightHandhj, false);
 
         // Same for the right side
-        float RightTrigger = Input.GetAxis(RTStr);
-        RightTrigger = Mathf.Approximately(RightTrigger, 0f) || Mathf.Approximately(RightTrigger, -1f) ? 0f : 1f;
+        float RightTrigger = Input.GetAxis (RTStr);
+        RightTrigger = Mathf.Approximately (RightTrigger, 0f) || Mathf.Approximately (RightTrigger, -1f) ? 0f : 1f;
 
 
         // Bend the body all together
         JointSpring tempjs = _chesthj.spring;
-        tempjs.targetPosition = (Mathf.Approximately(LeftTrigger, 1f) || Mathf.Approximately(RightTrigger, 1f) ? 1f : 0f) * 90f;
-        tempjs.targetPosition = Mathf.Clamp(tempjs.targetPosition, _chesthj.limits.min + 5, _chesthj.limits.max - 5);
+        tempjs.targetPosition = (Mathf.Approximately (LeftTrigger, 1f) || Mathf.Approximately (RightTrigger, 1f) ? 1f : 0f) * 90f;
+        tempjs.targetPosition = Mathf.Clamp (tempjs.targetPosition, _chesthj.limits.min + 5, _chesthj.limits.max - 5);
         _chesthj.spring = tempjs;
     }
 
-    private void CheckArmHelper(float TriggerValue, HingeJoint Arm2hj, HingeJoint Armhj, HingeJoint Handhj, bool IsLeftHand)
+    private void CheckArmHelper (float TriggerValue, HingeJoint Arm2hj, HingeJoint Armhj, HingeJoint Handhj, bool IsLeftHand)
     {
         // Arm2: Min -90 --> 89
         JointLimits lm1 = Arm2hj.limits;
         if (IsLeftHand)
-            lm1.min = Mathf.Approximately(TriggerValue, 1f) ? 89f : -90f;
+            lm1.min = Mathf.Approximately (TriggerValue, 1f) ? 89f : -90f;
         else
-            lm1.max = Mathf.Approximately(TriggerValue, 1f) ? -89f : 90f;
+            lm1.max = Mathf.Approximately (TriggerValue, 1f) ? -89f : 90f;
         Arm2hj.limits = lm1;
 
         // Arm: Connected Mass Scale 1 --> 0
-        Armhj.connectedMassScale = Mathf.Approximately(TriggerValue, 1f) ? 0f : 1f;
+        Armhj.connectedMassScale = Mathf.Approximately (TriggerValue, 1f) ? 0f : 1f;
 
         //  Arm: Limits: -90, 90 --> 81, 120
         JointLimits lm = Armhj.limits;
-        lm.max = Mathf.Approximately(TriggerValue, 1f) ? 120f : 90f;
-        lm.min = Mathf.Approximately(TriggerValue, 1f) ? 81 : -90f;
+        lm.max = Mathf.Approximately (TriggerValue, 1f) ? 120f : 90f;
+        lm.min = Mathf.Approximately (TriggerValue, 1f) ? 81 : -90f;
         Armhj.limits = lm;
 
         // Arm: Target Position: 0 --> 180
@@ -122,17 +122,17 @@ public class PlayerController : MonoBehaviour
     }
 
 
-    private void CheckBend()
+    private void CheckBend ()
     {
-        float VRAxis = Input.GetAxis("XboxVerticalRight") * -1f;
+        float VRAxis = Input.GetAxis ("XboxVerticalRight") * -1f;
 
         JointSpring js = _chesthj.spring;
         js.targetPosition = VRAxis * 90f;
-        js.targetPosition = Mathf.Clamp(js.targetPosition, _chesthj.limits.min + 5, _chesthj.limits.max - 5);
+        js.targetPosition = Mathf.Clamp (js.targetPosition, _chesthj.limits.min + 5, _chesthj.limits.max - 5);
         _chesthj.spring = js;
     }
 
-    private void CheckJump()
+    private void CheckJump ()
     {
         KeyCode JumpCode = KeyCode.A;
         switch (PlayerControllerNumber)
@@ -142,7 +142,7 @@ public class PlayerController : MonoBehaviour
                 JumpCode = KeyCode.Joystick1Button16;
 #endif
 #if UNITY_EDITOR_WIN
-         JumpCode = KeyCode.Joystick1Button0;
+                JumpCode = KeyCode.Joystick1Button0;
 #endif
                 break;
             case "2":
@@ -150,7 +150,7 @@ public class PlayerController : MonoBehaviour
                 JumpCode = KeyCode.Joystick2Button16;
 #endif
 #if UNITY_EDITOR_WIN
-         JumpCode = KeyCode.Joystick2Button0;
+                JumpCode = KeyCode.Joystick2Button0;
 #endif
                 break;
             case "3":
@@ -158,7 +158,7 @@ public class PlayerController : MonoBehaviour
                 JumpCode = KeyCode.Joystick3Button16;
 #endif
 #if UNITY_EDITOR_WIN
-         JumpCode = KeyCode.Joystick3Button0;
+                JumpCode = KeyCode.Joystick3Button0;
 #endif
                 break;
             case "4":
@@ -166,37 +166,37 @@ public class PlayerController : MonoBehaviour
                 JumpCode = KeyCode.Joystick4Button16;
 #endif
 #if UNITY_EDITOR_WIN
-         JumpCode = KeyCode.Joystick4Button0;
+                JumpCode = KeyCode.Joystick4Button0;
 #endif
                 break;
         }
 
-        if (Input.GetKeyDown(JumpCode) && IsGrounded())
+        if (Input.GetKeyDown (JumpCode) && IsGrounded ())
         {
-            _rb.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
+            _rb.AddForce (new Vector3 (0, JumpForce, 0), ForceMode.Impulse);
         }
     }
 
-    private void CheckMovement()
+    private void CheckMovement ()
     {
         string HLcontrollerStr = "Joy" + PlayerControllerNumber + "Axis1";
         string VLcontrollerStr = "Joy" + PlayerControllerNumber + "Axis2";
-        float HLAxis = Input.GetAxis(HLcontrollerStr);
-        float VLAxis = Input.GetAxis(VLcontrollerStr);
+        float HLAxis = Input.GetAxis (HLcontrollerStr);
+        float VLAxis = Input.GetAxis (VLcontrollerStr);
 
-        if (!Mathf.Approximately(HLAxis, 0f) || !Mathf.Approximately(VLAxis, 0f))
+        if (!Mathf.Approximately (HLAxis, 0f) || !Mathf.Approximately (VLAxis, 0f))
         {
             // Turn on the animator of the Leg Swing Preference
-            LegSwingReference.GetComponent<Animator>().enabled = true;
+            LegSwingReference.GetComponent<Animator> ().enabled = IsGrounded ();
             // Get the percent of input force player put in
-            float normalizedInputVal = Mathf.Sqrt(Mathf.Pow(HLAxis, 2f) + Mathf.Pow(VLAxis, 2f)) / Mathf.Sqrt(2);
+            float normalizedInputVal = Mathf.Sqrt (Mathf.Pow (HLAxis, 2f) + Mathf.Pow (VLAxis, 2f)) / Mathf.Sqrt (2);
             // Add force based on that percentage
-            _rb.GetComponent<Rigidbody>().AddForce(transform.forward * Thrust * normalizedInputVal);
+            _rb.GetComponent<Rigidbody> ().AddForce (transform.forward * Thrust * normalizedInputVal);
             // Turn player according to the rotation of the joystick
             //transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(HLAxis, VLAxis * -1f) * Mathf.Rad2Deg, transform.eulerAngles.z);
             float playerRot = transform.rotation.eulerAngles.y > 180f ? (transform.rotation.eulerAngles.y - 360f) : transform.rotation.eulerAngles.y;
-            float controllerRot = Mathf.Atan2(HLAxis, VLAxis * -1f) * Mathf.Rad2Deg;
-            if (!(Mathf.Abs(playerRot - controllerRot) < 20f))
+            float controllerRot = Mathf.Atan2 (HLAxis, VLAxis * -1f) * Mathf.Rad2Deg;
+            if (!(Mathf.Abs (playerRot - controllerRot) < 20f))
             {
                 //float mirrorAngle = playerRot > 0f ? (playerRot - 180f) : (playerRot + 180f);
                 //float rotation = controllerRot - playerRot > 0f ? 1f : -1f;
@@ -210,26 +210,26 @@ public class PlayerController : MonoBehaviour
             {
                 RotationSpeed = 4f;
             }
-            RotationSpeed = Mathf.Clamp(RotationSpeed, 4f, 15f);
-            Transform target = TurnReference.transform.GetChild(0);
+            RotationSpeed = Mathf.Clamp (RotationSpeed, 4f, 15f);
+            Transform target = TurnReference.transform.GetChild (0);
             Vector3 relativePos = target.position - transform.position;
 
-            TurnReference.transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(HLAxis, VLAxis * -1f) * Mathf.Rad2Deg, transform.eulerAngles.z);
-            Quaternion rotation = Quaternion.LookRotation(relativePos, Vector3.up);
-            Quaternion tr = Quaternion.Slerp(transform.rotation, rotation, Time.deltaTime * RotationSpeed);
+            TurnReference.transform.eulerAngles = new Vector3 (transform.eulerAngles.x, Mathf.Atan2 (HLAxis, VLAxis * -1f) * Mathf.Rad2Deg, transform.eulerAngles.z);
+            Quaternion rotation = Quaternion.LookRotation (relativePos, Vector3.up);
+            Quaternion tr = Quaternion.Slerp (transform.rotation, rotation, Time.deltaTime * RotationSpeed);
             transform.rotation = tr;
         }
         else
         {
-            LegSwingReference.GetComponent<Animator>().enabled = false;
+            LegSwingReference.GetComponent<Animator> ().enabled = false;
             LegSwingReference.transform.eulerAngles = Vector3.zero;
         }
     }
 
     #region Helper Functions
-    private bool IsGrounded()
+    private bool IsGrounded ()
     {
-        return Physics.Raycast(transform.position, -Vector3.up, _distToGround + 0.2f, JumpMask);
+        return Physics.Raycast (transform.position, -Vector3.up, _distToGround + 0.2f, JumpMask);
     }
     #endregion
 }

82de75d7ec1d1cbe166794cfe72778a71f7d7b72
Suyi Diao <suyi.diao@vanderbilt.edu>
2018-11-24 22:09:55 -0500

    did nothing

---
+++ Assets/Scripts/GunPositionControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunPositionControl : MonoBehaviour
+{
+
+    public GameObject Owner;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Owner != null)
+        {
+            GameObject lh = Owner.GetComponent<PlayerController>().LeftHand;
+            GameObject rh = Owner.GetComponent<PlayerController>().RightHand;
+            transform.position = (lh.transform.position + rh.transform.position) / 2f;
+            transform.rotation = Owner.transform.rotation;
+        }
+    }
+}

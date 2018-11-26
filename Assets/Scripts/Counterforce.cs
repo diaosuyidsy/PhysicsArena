18e372062f8f35629316e936731b999326eb425e
TATammy <burningtammy@gmail.com>
2018-11-26 01:17:29 -0500

    "Added a Counterforce script that can't be able to attach to watergun."

---
+++ Assets/Scripts/Counterforce.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+	public Rigidbody rb;
+	public float Thrust;
+	private float _fire;
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	private bool StartToFire()
+	{
+		return Mathf.Approximately(_fire, 1f);
+	}
+	
+	private void Update()
+	{
+		//float moveHorizontal = Input.GetAxis("Horizontal");
+		//float moveVerticle = Input.GetAxis("Vertical");
+
+		//Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVerticle);
+		
+		_fire = Input.GetAxis("XboxRT");
+		_fire = Mathf.Approximately(_fire, 0f) || Mathf.Approximately(_fire, -1f) ? 0f : 1f;
+		
+		if (StartToFire())
+		{
+			rb.AddRelativeForce(Vector3.back * Thrust);
+		}
+	}
+}

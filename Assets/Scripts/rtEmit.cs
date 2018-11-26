30f6e01be81c94dcf42c0330422e581af3e43e3a
diaosuyidsy <suyi.diao@vanderbilt.edu>
2018-11-26 03:13:57 -0500

    almost done

--- Assets/Scripts/rtEmit.cs
+++ Assets/Scripts/rtEmit.cs
@@ -7,10 +7,19 @@ public class rtEmit : MonoBehaviour
 {
     public GameObject WaterBall;
     public float Speed;
+    public float BackFireThrust;
+    public float UpThrust = 1f;
 
     public void Shoot (float TriggerVal)
     {
         WaterBall.GetComponent<ObiEmitter> ().speed = TriggerVal * Speed;
+        GunPositionControl gpc = GetComponent<GunPositionControl> ();
+        if (gpc != null)
+        {
+            gpc.Owner.GetComponent<Rigidbody> ().AddForce (-gpc.Owner.transform.forward * BackFireThrust * TriggerVal, ForceMode.Impulse);
+            gpc.Owner.GetComponent<Rigidbody> ().AddForce (gpc.Owner.transform.up * BackFireThrust * TriggerVal * UpThrust, ForceMode.Impulse);
+
+        }
     }
 
 }

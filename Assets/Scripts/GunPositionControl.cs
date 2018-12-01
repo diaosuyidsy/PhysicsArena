8b37e112f9fae1c854126db884ce0f64cde33721
diaosuyidsy <suyi.diao@vanderbilt.edu>
2018-12-01 16:11:57 -0500

    public gun position control angle

--- Assets/Scripts/GunPositionControl.cs
+++ Assets/Scripts/GunPositionControl.cs
@@ -6,20 +6,24 @@ public class GunPositionControl : MonoBehaviour
 {
     [HideInInspector]
     public GameObject Owner;
+    [Tooltip ("This Angle is for weapon z axis angle")]
+    public float DownAngle = 10f;
+    [Tooltip ("This Angle is for weapon y axis angle offset, normally it's 90f")]
+    public float FaceAngleOffset = 90f;
 
     // Update is called once per frame
-    void Update()
+    void Update ()
     {
         if (Owner != null)
         {
-            GameObject lh = Owner.GetComponent<PlayerController>().LeftHand;
-            GameObject rh = Owner.GetComponent<PlayerController>().RightHand;
+            GameObject lh = Owner.GetComponent<PlayerController> ().LeftHand;
+            GameObject rh = Owner.GetComponent<PlayerController> ().RightHand;
             transform.position = (lh.transform.position + rh.transform.position) / 2f;
-            transform.eulerAngles = new Vector3(0f, Owner.transform.eulerAngles.y + 90f, 10f);
+            transform.eulerAngles = new Vector3 (0f, Owner.transform.eulerAngles.y + 90f, DownAngle);
         }
     }
 
-    public void Drop()
+    public void Drop ()
     {
         Owner = null;
     }

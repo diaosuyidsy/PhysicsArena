e270b031aa31bf3df0256efc8dafe4ce02110860
diaosuyidsy <suyi.diao@vanderbilt.edu>
2018-12-09 00:25:31 -0500

    changed camera to smooth follow

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -7,6 +7,8 @@ public class CameraController : MonoBehaviour
     [HideInInspector]
     public Vector3 FollowTarget;
 
+    public float SmoothSpeed = 0.04f;
+
     public float CameraScaleSpeed = 1f;
     public float FOVSizeMin = 8f;
     public float FOVSizeMax = 15f;
@@ -14,15 +16,18 @@ public class CameraController : MonoBehaviour
     private float _maxDistanceOrigin;
     private float _xDiffOrigin;
     private float _zDiffOrigin;
+    private Vector3 _desiredPosition;
+    private Vector3 _smoothedPosition;
+
     // Use this for initialization
-    void Start()
+    void Start ()
     {
-        SetTarget();
+        SetTarget ();
         // Set the max Distance originally
         float maxDist = 0f;
         foreach (GameObject go in GameManager.GM.Players)
         {
-            float temp = Vector3.Distance(go.transform.position, FollowTarget);
+            float temp = Vector3.Distance (go.transform.position, FollowTarget);
             if (temp > maxDist)
             {
                 maxDist = temp;
@@ -35,16 +40,18 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        SetTarget();
-        transform.position = new Vector3(FollowTarget.x + _xDiffOrigin, transform.position.y, FollowTarget.z + _zDiffOrigin);
-        GetComponent<Camera>().fieldOfView += (MaxDistance() - _maxDistanceOrigin) * CameraScaleSpeed;
-        _maxDistanceOrigin = MaxDistance();
-        GetComponent<Camera>().fieldOfView = Mathf.Clamp(GetComponent<Camera>().fieldOfView, FOVSizeMin, FOVSizeMax);
+        SetTarget ();
+        _desiredPosition = new Vector3 (FollowTarget.x + _xDiffOrigin, transform.position.y, FollowTarget.z + _zDiffOrigin);
+        _smoothedPosition = Vector3.Lerp (transform.position, _desiredPosition, SmoothSpeed);
+        transform.position = _smoothedPosition;
+        GetComponent<Camera> ().fieldOfView += (MaxDistance () - _maxDistanceOrigin) * CameraScaleSpeed;
+        _maxDistanceOrigin = MaxDistance ();
+        GetComponent<Camera> ().fieldOfView = Mathf.Clamp (GetComponent<Camera> ().fieldOfView, FOVSizeMin, FOVSizeMax);
     }
 
-    void SetTarget()
+    void SetTarget ()
     {
         // Need to set Follow Target to be average of all players
         Vector3 total = Vector3.zero;
@@ -57,12 +64,12 @@ public class CameraController : MonoBehaviour
         FollowTarget = total;
     }
 
-    float MaxDistance()
+    float MaxDistance ()
     {
         float maxDist = 0f;
         foreach (GameObject go in GameManager.GM.Players)
         {
-            float temp = Vector3.Distance(go.transform.position, FollowTarget);
+            float temp = Vector3.Distance (go.transform.position, FollowTarget);
             if (temp > maxDist)
             {
                 maxDist = temp;

66d92bd4ff4bb3a05d0252601430dc32f1983102
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-01-04 22:39:47 -0500

    adding orisa ball sucker

--- Assets/Scripts/rtSuck.cs
+++ Assets/Scripts/rtSuck.cs
@@ -4,5 +4,36 @@ using UnityEngine;
 
 public class rtSuck : MonoBehaviour
 {
+    public float MaxBallTravelTime = 1f;
+    public float BallTravelSpeed = 3f;
 
+    private float _ballTraveledTime = 0f;
+    private GameObject _suckBall;
+    private enum State
+    {
+        In,
+        Out,
+        Suck
+    }
+
+    private State _ballState;
+
+    private void Start()
+    {
+        _ballState = State.In;
+        _suckBall = transform.GetChild(0).gameObject;
+    }
+
+    private void Update()
+    {
+        if (_ballState == State.Out)
+        {
+            _suckBall.transform.position += Time.deltaTime * _suckBall.transform.right * BallTravelSpeed;
+        }
+    }
+
+    public void Suck(bool holding)
+    {
+
+    }
 }

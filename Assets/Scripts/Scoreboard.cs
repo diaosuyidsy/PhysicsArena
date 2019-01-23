eb37c8f7916a712314e3965fdf428ae426e950f9
TATammy0w0 <wm1066@nyu.edu>
2019-01-23 12:34:38 -0500

    Added a Scoreboard script

---
+++ Assets/Scripts/Scoreboard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Scoreboard : MonoBehaviour
+{
+	public GameObject ChickensWin;
+	public GameObject DucksWin;
+	public GameObject MostKills;
+	public GameObject MostKillsName;
+	public GameObject MostSuicide;
+	public GameObject MostSuicideName;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

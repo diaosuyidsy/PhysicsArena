91019bcc094a89b5791f44ee3f3d808618b9c128
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-01-25 20:53:52 -0500

    fixed minor bugs

--- Assets/Scripts/WeaponGenerationManager.cs
+++ Assets/Scripts/WeaponGenerationManager.cs
@@ -36,27 +36,27 @@ public class WeaponGenerationManager : MonoBehaviour
         // If next weapon in array is deactivated
         // Then move it to the current random spawn location
         // Then activate it
-        while (_nextspawnamount > 0)
-        {
-            System.Random rand = new System.Random();
-            int rannum = rand.Next(0, Weapons.Length);
-            GameObject weapon = Weapons[rannum];
-            if (!weapon.activeSelf)
-            {
-                GameManager.GM.MoveWeaponToSpawnArea(weapon);
-                weapon.SetActive(true);
-                _nextspawnamount--;
-            }
-        }
-        //foreach (GameObject weapon in Weapons)
+        //while (_nextspawnamount > 0)
         //{
-        //    if (!weapon.activeSelf && _nextspawnamount > 0)
+        //    System.Random rand = new System.Random();
+        //    int rannum = rand.Next(0, Weapons.Length);
+        //    GameObject weapon = Weapons[rannum];
+        //    if (!weapon.activeSelf)
         //    {
         //        GameManager.GM.MoveWeaponToSpawnArea(weapon);
         //        weapon.SetActive(true);
         //        _nextspawnamount--;
         //    }
         //}
+        foreach (GameObject weapon in Weapons)
+        {
+            if (!weapon.activeSelf && _nextspawnamount > 0)
+            {
+                GameManager.GM.MoveWeaponToSpawnArea(weapon);
+                weapon.SetActive(true);
+                _nextspawnamount--;
+            }
+        }
 
         // If we want to change next spawn amount accordingly, do it here
         _nextspawnamount = NextSpawnAmount;

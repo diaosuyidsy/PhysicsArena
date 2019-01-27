ac2ac64f10ebb28f8a4c4be8a752e5cacacdfa5b
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-01-27 15:02:53 -0500

    changed nothing but I still want to upload

---
+++ Assets/Scripts/CountDownVideoPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class CountDownVideoPlayer : MonoBehaviour
+{
+    public RawImage rawImage;
+    private VideoPlayer videoplayer;
+
+    private void Start()
+    {
+        videoplayer = GetComponent<VideoPlayer>();
+        StartCoroutine(PlayVideo());
+    }
+
+    IEnumerator PlayVideo()
+    {
+        videoplayer.Prepare();
+        WaitForSeconds wfs = new WaitForSeconds(1f);
+        while (!videoplayer.isPrepared)
+        {
+            yield return wfs;
+            break;
+        }
+        rawImage.texture = videoplayer.texture;
+        videoplayer.Play();
+
+    }
+
+}

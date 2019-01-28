48f86b4595f74953988bb9e2503f34a5f73e27a3
TATammy0w0 <wm1066@nyu.edu>
2019-01-28 18:03:20 -0500

    Added an intro scene.

---
+++ Assets/Scripts/GameManagerStart.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagerStart : MonoBehaviour
+{
+	public static GameManagerStart GMS;
+	public GameObject WinningObj;
+	public GameObject Payload;
+	public GameObject Or;
+	public GameObject Food;
+
+	private void Awake()
+	{
+		GMS = this;
+	}
+
+	private IEnumerator PlayIntro()
+	{
+		yield return new WaitForSeconds(1f);
+		
+		WinningObj.SetActive(true);
+		
+		yield return new WaitForSeconds(5f);
+		
+		Payload.SetActive(true);
+		
+		yield return new WaitForSeconds(7f);
+		
+		Or.SetActive(true);
+		
+		yield return new WaitForSeconds(3f);
+		
+		Food.SetActive(true);
+		
+		yield return new WaitForSeconds(3f);
+
+		SceneManager.LoadSceneAsync(1);
+	}
+
+	public void StartIntro()
+	{
+		StartCoroutine(PlayIntro());
+	}
+}

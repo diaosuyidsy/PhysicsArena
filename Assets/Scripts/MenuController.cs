8ba07b98d55bab309629f921435fd0dc8b040edb
U-DESKTOP-11UIFRG\diaos <suyi.diao@vanderbilt.edu>
2019-01-28 00:25:36 -0500

    added

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This Class is for the entire 2Dstartscene progress
+public class MenuController : MonoBehaviour
+{
+    public GameObject CanvasManager;
+
+    private enum State
+    {
+        OnStartImage,
+        OnCharacterSelection,
+        OnLoading,
+    }
+
+    private State MenuState;
+
+    // Use this for initialization
+    void Start()
+    {
+        CanvasManager.SetActive(false);
+        MenuState = State.OnStartImage;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _checkSelectionInput();
+    }
+
+    private void _checkSelectionInput()
+    {
+        if (MenuState != State.OnStartImage) return;
+    }
+}

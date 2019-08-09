4cd43db006c8eedf9a5203f644560090b75082a6
Suyi Diao <suyi.diao@vanderbilt.edu>
2019-08-09 00:00:37 -0400

    Added Menu Sound Slot

---
+++ Assets/Scripts/Scriptableobjects/MenuAudioData.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "MenuAudioData", menuName = "ScriptableObjects/MenuAudioData", order = 1)]
+public class MenuAudioData : ScriptableObject
+{
+    public AudioClip BrowseAudioClip;
+    public AudioClip SelectionAudioClip;
+    public AudioClip SecondMenuModeDropDownAudioClip;
+    public AudioClip SecondMenuModeDropUpAudioClip;
+    public AudioClip SecondMenuMapBrowseAudioClip;
+    public AudioClip BackAudioClip;
+    public AudioClip ThirdMenuSlotInAudioClip;
+    public AudioClip ThirdMenuSlotOutAudioClip;
+    public AudioClip ThirdMenuJoinGameAudioClip;
+    public AudioClip ThirdMenuEggJiggleAudioClip;
+    public AudioClip ThirdMenuUnJoinGameAudioClip;
+    public AudioClip ThirdMenuEggSelectedAudioClip;
+    public AudioClip ThirdMenuEggUpAudioClip;
+    public AudioClip ThirdMenuChickenLandAudioClip;
+    public AudioClip ThirdMenuGroundMoveBackAudioClip;
+    public AudioClip ThirdMenuEggDisappearAudioClip;
+    public AudioClip ThirdMenuChickenToEggAudioClip;
+    public AudioClip ThirdMenuChickenToAbyssAudioClip;
+
+}

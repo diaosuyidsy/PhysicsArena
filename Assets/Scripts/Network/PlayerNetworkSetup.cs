449d0a07740bf61efc67b591ab29b4f2b365a7d7
Suyi Diao <suyi.diao@vanderbilt.edu>
2019-08-17 01:49:09 -0400

    Smooth Sync Movement

---
+++ Assets/Scripts/Network/PlayerNetworkSetup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class PlayerNetworkSetup : NetworkBehaviour
+{
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<PlayerControllerNetworking>().enabled = true;
+    }
+}

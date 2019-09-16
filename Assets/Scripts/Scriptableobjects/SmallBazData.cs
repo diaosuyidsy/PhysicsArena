25911999ace0a3b9282e57bd8bd33f3a1067beea
Suyi Diao <suyi.diao@vanderbilt.edu>
2019-09-16 10:58:53 -0400

    Added Small Baz Prototype

--- Assets/Scripts/Scriptableobjects/SmallBazData.cs
+++ Assets/Scripts/Scriptableobjects/SmallBazData.cs
@@ -6,16 +6,17 @@ using DG.Tweening;
 [CreateAssetMenu(fileName = "SmallBazData", menuName = "ScriptableObjects/Weapon/SmallBazData", order = 1)]
 public class SmallBazData : WeaponDataBase
 {
-	public float BoomerangSpeed = 9f;
-	public float OnHitForce = 900f;
-	public float CurveMultiplier = 1f;
-	public Vector3[] LocalMovePoints;
-	public float BoomerangYOffset;
-	public AnimationCurve BoomEase;
+	public float Radius;
+	public float OnHitForce = 1200f;
+	public float UpwardFinalY = 10f;
+	public float UpwardDuration = 0.5f;
+	public Ease UpwardEase = Ease.OutQuad;
+	public float InAirDuration = 2f;
+	public float DownwardAccelaration = 5f;
 	public int MaxAmmo = 1;
-	public LayerMask CanHitLayer;
-	public LayerMask ObstacleLayer;
-	[Header("Hit Scan Config")]
-	public float HitRadius = 0.5f;
-	public float HitMaxDistance = 0.1f;
+	public LayerMask HitExplodeLayer;
+	public LayerMask CanHideLayer;
+	//[Header("Hit Scan Config")]
+	//public float HitRadius = 0.5f;
+	//public float HitMaxDistance = 0.1f;
 }
--- Assets/Scripts/Weapons/rtSmallBaz.cs
+++ Assets/Scripts/Weapons/rtSmallBaz.cs
@@ -10,56 +10,80 @@ public class rtSmallBaz : WeaponBase
 		In,
 		Out,
 	}
-	private SmallBazState _boomerangState;
-	private Tweener _pathMoveTweener;
+	private SmallBazState _smallBazState;
 	private GameObject _firer;
-	private HashSet<PlayerController> _hitSet;
+	private SmallBazData _weaponData;
 
 	protected override void Awake()
 	{
 		base.Awake();
-		_ammo = WeaponDataStore.BoomerangDataStore.MaxAmmo;
-		_boomerangState = SmallBazState.In;
-		_hitSet = new HashSet<PlayerController>();
+		_weaponData = WeaponDataBase as SmallBazData;
+		_ammo = _weaponData.MaxAmmo;
+		_smallBazState = SmallBazState.In;
 	}
 
 	protected override void Update()
 	{
 		base.Update();
-		if (_boomerangState == SmallBazState.Out)
+		if (_smallBazState == SmallBazState.Out)
 		{
+			RaycastHit hit;
+			if (Physics.SphereCast(transform.position, 0.5f, Vector3.down, out hit, 0.5f, _weaponData.HitExplodeLayer))
+			{
+				List<GameObject> affectedPlayers = new List<GameObject>();
+				// Hit Ground/Player/Obstacle
+				foreach (PlayerController _pc in Services.GameStateManager.PlayerControllers)
+				{
+					float dis = Vector3.Distance(_pc.transform.position, transform.position);
+					if (_pc.gameObject.activeInHierarchy &&
+						dis < _weaponData.Radius &&
+						_pc.gameObject != Owner &&
+						!Physics.Linecast(_pc.transform.position, transform.position, _weaponData.CanHideLayer))
+					{
+						affectedPlayers.Add(_pc.gameObject);
+						Vector3 dir = _pc.transform.position - transform.position;
+						dir.y = 0f;
+						_pc.OnImpact(_weaponData.OnHitForce * dir.normalized, ForceMode.Impulse, _firer, ImpactType.SmallBaz);
+					}
+				}
+				EventManager.Instance.TriggerEvent(new BazookaBombed(gameObject, _firer, _firer.GetComponent<PlayerController>().PlayerNumber, affectedPlayers));
+				_smallBazState = SmallBazState.In;
+			}
+		}
+	}
 
+	private void FixedUpdate()
+	{
+		if (_smallBazState == SmallBazState.Out)
+		{
+			GetComponent<Rigidbody>().AddForce(Vector3.down * _weaponData.DownwardAccelaration);
 		}
 	}
 
 	public override void Fire(bool buttondown)
 	{
-		if (!buttondown)
+		if (buttondown)
 		{
 			_firer = Owner;
-			_boomerangState = SmallBazState.Out;
-			Vector3[] localPath = new Vector3[WeaponDataStore.BoomerangDataStore.LocalMovePoints.Length];
-			for (int i = 0; i < WeaponDataStore.BoomerangDataStore.LocalMovePoints.Length; i++)
-			{
-				localPath[i] = transform.position + transform.forward * WeaponDataStore.BoomerangDataStore.LocalMovePoints[i].z + transform.right * WeaponDataStore.BoomerangDataStore.LocalMovePoints[i].x;
-			}
-			_pathMoveTweener = transform.DOLocalPath(localPath, WeaponDataStore.BoomerangDataStore.BoomerangSpeed, PathType.CatmullRom)
-			.SetSpeedBased(true)
-			.SetEase(WeaponDataStore.BoomerangDataStore.BoomEase)
-			.SetLookAt(0f)
-			.OnComplete(() =>
-			{
-				GetComponent<Rigidbody>().velocity = transform.forward * WeaponDataStore.BoomerangDataStore.BoomerangSpeed;
-			});
 			_onWeaponUsedOnce();
+			GetComponent<Rigidbody>().isKinematic = true;
+			Vector3 UpwardFinalPos = new Vector3(transform.localPosition.x, _weaponData.UpwardFinalY, transform.localPosition.z);
+			Sequence seq = DOTween.Sequence();
+			seq.Append(transform.DOLocalMove(UpwardFinalPos, _weaponData.UpwardDuration).SetEase(_weaponData.UpwardEase));
+			seq.AppendCallback(() =>
+			{
+				GetComponent<Rigidbody>().isKinematic = false;
+				GetComponent<Rigidbody>().velocity = Vector3.zero;
+				GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
+				_smallBazState = SmallBazState.Out;
+			});
 		}
 	}
 
 	protected override void _onWeaponDespawn()
 	{
-		_boomerangState = SmallBazState.In;
-		_ammo = WeaponDataStore.BoomerangDataStore.MaxAmmo;
-		_hitSet.Clear();
+		_smallBazState = SmallBazState.In;
+		_ammo = _weaponData.MaxAmmo;
 		EventManager.Instance.TriggerEvent(new ObjectDespawned(gameObject));
 		gameObject.SetActive(false);
 	}

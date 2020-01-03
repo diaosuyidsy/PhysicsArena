2ec2fe275181b71b543cda25ed3bd36c35cf9899
Suyi Diao <suyi.diao@vanderbilt.edu>
2020-01-03 13:32:31 -0500

    Added Tools; 三轴转体

---
+++ Assets/Scripts/Tools/OpenSaveFile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+
+public class OpenSaveFile : MonoBehaviour
+{
+    [MenuItem("Tools/Birfia/Open Save File")]
+    private static void Apply()
+    {
+        Debug.Log("Open Save File");
+        string path = Path.Combine(Application.persistentDataPath, "data");
+        path = Path.Combine(path, "PlayersInformation.txt");
+        if (!Directory.Exists(Path.GetDirectoryName(path)))
+        {
+            Debug.LogWarning("Save Directory Not Exits Yet");
+            return;
+        }
+        System.Diagnostics.Process.Start(path);
+    }
+}

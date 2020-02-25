7b45b3a30ce6c42283797a194b53d5ab0aca9486
Chang <cl4929@nyu.edu>
2020-02-25 00:30:00 -0500

    Update placehorlders and add a lot of representations. Fix bugs.

---
+++ Assets/Scripts/Chang/Reforged/CanonFeelData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanonFeelData : MonoBehaviour
+{
+    public float MaxLeverAngle;
+    public float MaxPipeAngle;
+    public float LeverRotateSpeed;
+    public float PipeRotateSpeed;
+
+    public Vector3 PipeEndStartLocalPos;
+    public float PipeEndFireTime;
+    public float PipeEndRecoverTime;
+    public float PipeEndShakeDis;
+
+    public float MarkAppearTime;
+    public Color MarkDefaultColor;
+    public Color MarkAlertColor;
+
+    public GameObject BombPrefab;
+    public float BombInvisibleHeight;
+    public float BombRiseTime;
+    public float BombFallTime;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

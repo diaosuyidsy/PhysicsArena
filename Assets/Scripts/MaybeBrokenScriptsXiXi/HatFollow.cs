1253c998e6db9b2ed3dfa22093ed9786079a2217
TATammy0w0 <tammy.ma@nyu.edu>
2020-02-27 09:40:51 -0500

    有种上传成功了的感觉！

---
+++ Assets/Scripts/MaybeBrokenScriptsXiXi/HatFollow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatFollow : MonoBehaviour
+{
+    public Transform Target;
+    public Vector3 Offset;
+    public float SmoothSpeed = 0.125f;
+
+    void LateUpdate()
+    {
+        Vector3 desiredPos = Target.position + Offset;
+        Vector3 smoothedPos = Vector3.Lerp (transform.position, desiredPos, SmoothSpeed);
+        transform.position = smoothedPos;
+    }
+
+}

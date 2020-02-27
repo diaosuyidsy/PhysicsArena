61197e4019911d79b490c10934fce22127f592be
Suyi Diao <suyi.diao@vanderbilt.edu>
2020-02-27 08:16:56 -0500

    Started

---
+++ Assets/Scripts/Menu/ComicMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ComicMenu : MonoBehaviour
+{
+    private FSM<ComicMenu> ComicMenuFSM;
+
+    private void Awake()
+    {
+        ComicMenuFSM = new FSM<ComicMenu>(this);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ComicMenuFSM.Update();
+    }
+
+    private abstract class MenuState : FSM<ComicMenu>.State
+    { }
+
+    private class FirstMenuState : MenuState
+    {
+
+    }
+}

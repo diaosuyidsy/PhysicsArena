f438c891efc70e4e3493a76c89e15254315e3b6b
Suyi Diao <suyi.diao@vanderbilt.edu>
2020-03-07 20:56:11 -0500

    Chicken Shield Done

--- Assets/Scripts/ShieldController.cs
+++ Assets/Scripts/ShieldController.cs
@@ -7,6 +7,9 @@ public class ShieldController : MonoBehaviour
     public Animator Animator;
     public Renderer[] ShieldRenderers;
     public GameObject Shield;
+    public GameObject ShieldCrack;
+
+    private GameObject _instantiatedShieldCrack;
 
     public void SetShield(bool open)
     {
@@ -20,5 +23,26 @@ public class ShieldController : MonoBehaviour
         {
             r.material.SetFloat("_Energy", Energy);
         }
+        if (Energy == 0f)
+        {
+            Shield.SetActive(false);
+            _instantiatedShieldCrack = Instantiate(ShieldCrack, transform.parent, true);
+            _instantiatedShieldCrack.SetActive(true);
+            _instantiatedShieldCrack.transform.parent = null;
+            StartCoroutine(increaseShieldCraftDistance(0.5f));
+        }
+    }
+
+    IEnumerator increaseShieldCraftDistance(float time)
+    {
+        float elapsedTime = 0f;
+        while (elapsedTime < time)
+        {
+            _instantiatedShieldCrack.GetComponent<Renderer>().material.SetFloat("_Distance", elapsedTime / time);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        Destroy(_instantiatedShieldCrack);
+        _instantiatedShieldCrack = null;
     }
 }

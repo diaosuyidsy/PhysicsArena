e478c1b179a9f2f8df9f077c1a2608054ae9dc04
Chang <cl4929@nyu.edu>
2020-03-10 00:23:51 -0400

    85%

--- Assets/CartModeReforgedArenaManager.cs
+++ Assets/CartModeReforgedArenaManager.cs
@@ -15,24 +15,36 @@ public class CartModeReforgedArenaManager : MonoBehaviour
         Team2
     }
 
-    public CartModeReforgedModeData Data;
+    private enum CheckPointSide
+    {
+        Team1,
+        Team2
+    }
 
+    public CartModeReforgedModeData Data;
+    public CartModeReforgedFeelData FeelData;
+
+    public GameObject Players;
     public GameObject Cart;
     public GameObject CheckPoints;
     public GameObject Team1ExpCounter;
     public GameObject Team1LevelText;
+    public GameObject Team1ExpText;
     public GameObject Team2ExpCounter;
     public GameObject Team2LevelText;
+    public GameObject Team2ExpText;
+
+    public float CartRadius;
+    public GameObject CartDotline;
 
 
-    public Color NeutralDotlineColor;
-    public Color Team1DotlineColor;
-    public Color Team2DotlineColor;
-
-    private List<GameObject> InCartPlayerList;
     private List<GameObject> CheckPointList;
     private int TargetWayPointIndex;
+    private int CurrentTeam1Checkpoints;
+    private int CurrentTeam2Checkpoints;
     private CartSide CurrentSide;
+    private CartSide LastSide;
+    private float CurrentSpeed;
 
 
     private int Team1Level;
@@ -40,6 +52,12 @@ public class CartModeReforgedArenaManager : MonoBehaviour
     private int Team1Exp;
     private int Team2Exp;
 
+    private float Team1LevelTextHopTimer;
+    private float Team1ExpTextHopTimer;
+    private float Team2LevelTextHopTimer;
+    private float Team2ExpTextHopTimer;
+    private int Team1AccumulatedExp;
+    private int Team2AccumulatedExp;
 
     private bool GameStart;
     private bool GameEnd;
@@ -47,18 +65,349 @@ public class CartModeReforgedArenaManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        EventManager.Instance.AddHandler<GameStart>(OnGameStart);
+        EventManager.Instance.AddHandler<PlayerDied>(OnPlayerDied);
+
+        TargetWayPointIndex = -1;
+        CurrentSide = LastSide = CartSide.Neutral;
+        CartDotline.GetComponent<SpriteRenderer>().color = FeelData.NeutralDotlineColor;
+
+        Team1LevelTextHopTimer = FeelData.TextHopTime;
+        Team1ExpTextHopTimer = FeelData.TextHopTime + FeelData.ExpTextStayTime;
+        Team2LevelTextHopTimer = FeelData.TextHopTime;
+        Team2ExpTextHopTimer = FeelData.TextHopTime + FeelData.ExpTextStayTime;
+
+        InitCheckPoint();
+    }
+
+    private void OnDestroy()
+    {
+        EventManager.Instance.RemoveHandler<GameStart>(OnGameStart);
+        EventManager.Instance.RemoveHandler<PlayerDied>(OnPlayerDied);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        DetectCartSide();
+        SetDotline();
+        CartMove();
+        SetTextHop();
     }
 
     private void DetectCartSide()
     {
+        int Team1Count = 0;
+        int Team2Count = 0;
 
+        foreach(Transform child in Players.transform)
+        {
+            if(child.gameObject.activeSelf && PlayerInCart(child.gameObject))
+            {
+                if (child.tag.Contains("1"))
+                {
+                    Team1Count++;
+                }
+                else
+                {
+                    Team2Count++;
+                }
+            }
+        }
+        if (CurrentSide != CartSide.Neutral)
+        {
+            LastSide = CurrentSide;
+        }
+        if (Team1Count>0 && Team2Count > 0)
+        {
+            if(Team1Level > Team2Level)
+            {
+                CurrentSide = CartSide.Team1;
+                CurrentSpeed = Data.BaseCartSpeed + Data.CartSpeedBonusPerLevel * (Team1Level-Team2Level);
+            }
+            else if (Team2Level > Team1Level)
+            {
+                CurrentSide = CartSide.Team2;
+                CurrentSpeed = Data.BaseCartSpeed + Data.CartSpeedBonusPerLevel * (Team2Level - Team1Level);
+            }
+            else
+            {
+                CurrentSide = CartSide.Neutral;
+                CurrentSpeed = 0;
+            }
+        }
+        else if (Team1Count > 0)
+        {
+            CurrentSide = CartSide.Team1;
+            CurrentSpeed = Data.BaseCartSpeed + Data.CartSpeedBonusPerLevel * Team1Level;
+        }
+        else if (Team2Count > 0)
+        {
+            CurrentSide = CartSide.Team2;
+            CurrentSpeed = Data.BaseCartSpeed + Data.CartSpeedBonusPerLevel * Team2Level;
+        }
+        else
+        {
+            CurrentSide = CartSide.Neutral;
+            CurrentSpeed = 0;
+        }
+    }
+
+    private void SetDotline()
+    {
+        switch (CurrentSide)
+        {
+            case CartSide.Neutral:
+                CartDotline.GetComponent<SpriteRenderer>().color = FeelData.NeutralDotlineColor;
+                break;
+            case CartSide.Team1:
+                CartDotline.GetComponent<SpriteRenderer>().color = FeelData.Team1DotlineColor;
+                break;
+            case CartSide.Team2:
+                CartDotline.GetComponent<SpriteRenderer>().color = FeelData.Team2DotlineColor;
+                break;
+        }
+    }
+
+    private void CartMove()
+    {
+        if(CurrentSide == CartSide.Team1)
+        {
+            if(TargetWayPointIndex < 0)
+            {
+                TargetWayPointIndex = CheckPointList.Count / 2;
+            }
+            else if(LastSide == CartSide.Team2)
+            {
+                TargetWayPointIndex++;
+            }
+        }
+        else if(CurrentSide == CartSide.Team2)
+        {
+            if (TargetWayPointIndex < 0)
+            {
+                TargetWayPointIndex = CheckPointList.Count / 2 - 1;
+            }
+            else if(LastSide == CartSide.Team1)
+            {
+                TargetWayPointIndex--;
+            }
+        }
+        else
+        {
+            return;
+        }
+
+        Vector3 Offset = CheckPointList[TargetWayPointIndex].transform.position - Cart.transform.position;
+        Offset.y = 0;
+
+        Cart.transform.position += CurrentSpeed * Offset.normalized*Time.deltaTime;
+        Cart.transform.forward = Offset.normalized;
+
+        if(Vector3.Dot(CheckPointList[TargetWayPointIndex].transform.position - Cart.transform.position, Offset)<0)
+        {
+            Cart.transform.position = new Vector3(CheckPointList[TargetWayPointIndex].transform.position.x, Cart.transform.position.y, CheckPointList[TargetWayPointIndex].transform.position.z);
+            if(CurrentSide == CartSide.Team1)
+            {
+                CurrentTeam1Checkpoints++;
+                CurrentTeam2Checkpoints--;
+                if(CurrentTeam2Checkpoints == 0)
+                {
+                    GameEnd = true;
+                    EventManager.Instance.TriggerEvent(new GameEnd(1, Cart.transform, GameWinType.CartWin));
+                }
+                else
+                {
+                    TargetWayPointIndex++;
+                }
+            }
+            else if(CurrentSide == CartSide.Team2)
+            {
+                CurrentTeam1Checkpoints--;
+                CurrentTeam2Checkpoints++;
+                if (CurrentTeam1Checkpoints == 0)
+                {
+                    GameEnd = true;
+                    EventManager.Instance.TriggerEvent(new GameEnd(2, Cart.transform, GameWinType.CartWin));
+                }
+                else
+                {
+                    TargetWayPointIndex--;
+                }
+            }
+
+            SetCheckPointAppearance();
+        }
+
+
+    }
+
+    private void InitCheckPoint()
+    {
+        CheckPointList = new List<GameObject>();
+
+        foreach(Transform child in CheckPoints.transform)
+        {
+            CheckPointList.Add(child.gameObject);
+        }
+
+        CurrentTeam1Checkpoints = CurrentTeam2Checkpoints = CheckPointList.Count / 2;
+
+        SetCheckPointAppearance();
+    }
+
+    private void SetTextHop()
+    {
+        Team1LevelTextHopTimer += Time.deltaTime;
+        Team1ExpTextHopTimer += Time.deltaTime;
+        Team2ExpTextHopTimer += Time.deltaTime;
+        Team2LevelTextHopTimer += Time.deltaTime;
+
+        if(Team1LevelTextHopTimer <= FeelData.TextHopTime/2)
+        {
+            Team1LevelText.transform.localScale = Vector3.one * Mathf.Lerp(FeelData.TextDefaultScale, FeelData.TextHopScale, Team1LevelTextHopTimer / (FeelData.TextHopTime / 2));
+        }
+        else if(Team1LevelTextHopTimer <= FeelData.TextHopTime)
+        {
+            Team1LevelText.transform.localScale = Vector3.one * Mathf.Lerp(FeelData.TextHopScale, FeelData.TextDefaultScale,  (Team1LevelTextHopTimer- FeelData.TextHopTime / 2) / (FeelData.TextHopTime / 2));
+        }
+        else
+        {
+            Team1LevelText.transform.localScale = Vector3.one * FeelData.TextDefaultScale;
+        }
+
+        if (Team2LevelTextHopTimer <= FeelData.TextHopTime / 2)
+        {
+            Team2LevelText.transform.localScale = Vector3.one * Mathf.Lerp(FeelData.TextDefaultScale, FeelData.TextHopScale, Team2LevelTextHopTimer / (FeelData.TextHopTime / 2));
+        }
+        else if (Team2LevelTextHopTimer <= FeelData.TextHopTime)
+        {
+            Team2LevelText.transform.localScale = Vector3.one * Mathf.Lerp(FeelData.TextHopScale, FeelData.TextDefaultScale,  (Team2LevelTextHopTimer - FeelData.TextHopTime / 2) / (FeelData.TextHopTime / 2));
+        }
+        else
+        {
+            Team2LevelText.transform.localScale = Vector3.one * FeelData.TextDefaultScale;
+        }
+
+        if(Team1ExpTextHopTimer <= FeelData.TextHopTime / 2)
+        {
+            Team1ExpText.GetComponent<TextMeshProUGUI>().enabled = true;
+            Team1ExpText.transform.localScale = Vector3.one * Mathf.Lerp(FeelData.TextDefaultScale, FeelData.TextHopScale, Team1ExpTextHopTimer / (FeelData.TextHopTime / 2));
+        }
+        else if(Team1ExpTextHopTimer <= FeelData.TextHopTime)
+        {
+            Team1ExpText.GetComponent<TextMeshProUGUI>().enabled = true;
+            Team1ExpText.transform.localScale = Vector3.one * Mathf.Lerp(FeelData.TextHopScale, FeelData.TextDefaultScale,  (Team1ExpTextHopTimer- FeelData.TextHopTime / 2) / (FeelData.TextHopTime / 2));
+        }
+        else if(Team1ExpTextHopTimer <= FeelData.TextHopTime + FeelData.ExpTextStayTime)
+        {
+            Team1ExpText.GetComponent<TextMeshProUGUI>().enabled = true;
+            Team1ExpText.transform.localScale = Vector3.one * FeelData.TextDefaultScale;
+        }
+        else
+        {
+            Team1ExpText.GetComponent<TextMeshProUGUI>().enabled = false;
+            Team1ExpText.transform.localScale = Vector3.one * FeelData.TextDefaultScale;
+            Team1AccumulatedExp = 0;
+        }
+
+        if (Team2ExpTextHopTimer <= FeelData.TextHopTime / 2)
+        {
+            Team2ExpText.GetComponent<TextMeshProUGUI>().enabled = true;
+            Team2ExpText.transform.localScale = Vector3.one * Mathf.Lerp(FeelData.TextDefaultScale, FeelData.TextHopScale, Team2ExpTextHopTimer / (FeelData.TextHopTime / 2));
+        }
+        else if (Team2ExpTextHopTimer <= FeelData.TextHopTime)
+        {
+            Team2ExpText.GetComponent<TextMeshProUGUI>().enabled = true;
+            Team2ExpText.transform.localScale = Vector3.one * Mathf.Lerp(FeelData.TextHopScale, FeelData.TextDefaultScale,  (Team2ExpTextHopTimer - FeelData.TextHopTime / 2) / (FeelData.TextHopTime / 2));
+        }
+        else if (Team2ExpTextHopTimer <= FeelData.TextHopTime + FeelData.ExpTextStayTime)
+        {
+            Team2ExpText.GetComponent<TextMeshProUGUI>().enabled = true;
+            Team2ExpText.transform.localScale = Vector3.one * FeelData.TextDefaultScale;
+        }
+        else
+        {
+            Team2ExpText.GetComponent<TextMeshProUGUI>().enabled = false;
+            Team2ExpText.transform.localScale = Vector3.one * FeelData.TextDefaultScale;
+            Team2AccumulatedExp = 0;
+        }
+    }
+
+    private void SetUI()
+    {
+        Team1LevelText.GetComponent<TextMeshProUGUI>().text = Team1Level.ToString();
+        Team1ExpText.GetComponent<TextMeshProUGUI>().text = "+" + Team1AccumulatedExp.ToString();
+        Team1ExpCounter.GetComponent<Image>().fillAmount = (float)Team1Exp / Data.LevelUpExp;
+
+        Team2LevelText.GetComponent<TextMeshProUGUI>().text = Team2Level.ToString();
+        Team2ExpText.GetComponent<TextMeshProUGUI>().text = "+" + Team2AccumulatedExp.ToString();
+        Team2ExpCounter.GetComponent<Image>().fillAmount = (float)Team2Exp / Data.LevelUpExp;
+    }
+
+    private void SetCheckPointAppearance()
+    {
+        if (CurrentTeam1Checkpoints == CurrentTeam2Checkpoints)
+        {
+            for (int i = 0; i < CurrentTeam1Checkpoints; i++)
+            {
+                CheckPointList[i].GetComponent<Renderer>().material = FeelData.EmissiveRed;
+                Material mat = CheckPointList[i].GetComponent<Renderer>().material;
+                mat.EnableKeyword("_EMISSION");
+                mat.SetColor("_EmissionColor", FeelData.RedCheckpointColor * FeelData.DeactivatedEmission);
+            }
+
+            for (int i = CurrentTeam1Checkpoints; i < CheckPointList.Count; i++)
+            {
+                CheckPointList[i].GetComponent<Renderer>().material = FeelData.EmissiveBlue;
+                Material mat = CheckPointList[i].GetComponent<Renderer>().material;
+                mat.EnableKeyword("_EMISSION");
+                mat.SetColor("_EmissionColor", FeelData.BlueCheckpointColor * FeelData.DeactivatedEmission);
+            }
+        }
+        else if(CurrentTeam1Checkpoints > CurrentTeam2Checkpoints)
+        {
+            for (int i = 0; i < CurrentTeam1Checkpoints; i++)
+            {
+                CheckPointList[i].GetComponent<Renderer>().material = FeelData.EmissiveRed;
+                Material mat = CheckPointList[i].GetComponent<Renderer>().material;
+                mat.EnableKeyword("_EMISSION");
+                mat.SetColor("_EmissionColor", FeelData.RedCheckpointColor * FeelData.ActivatedEmission);
+            }
+
+            for (int i = CurrentTeam1Checkpoints; i < CheckPointList.Count; i++)
+            {
+                CheckPointList[i].GetComponent<Renderer>().material = FeelData.EmissiveBlue;
+                Material mat = CheckPointList[i].GetComponent<Renderer>().material;
+                mat.EnableKeyword("_EMISSION");
+                mat.SetColor("_EmissionColor", FeelData.BlueCheckpointColor * FeelData.DeactivatedEmission);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < CurrentTeam1Checkpoints; i++)
+            {
+                CheckPointList[i].GetComponent<Renderer>().material = FeelData.EmissiveRed;
+                Material mat = CheckPointList[i].GetComponent<Renderer>().material;
+                mat.EnableKeyword("_EMISSION");
+                mat.SetColor("_EmissionColor", FeelData.RedCheckpointColor * FeelData.DeactivatedEmission);
+            }
+
+            for (int i = CurrentTeam1Checkpoints; i < CheckPointList.Count; i++)
+            {
+                CheckPointList[i].GetComponent<Renderer>().material = FeelData.EmissiveBlue;
+                Material mat = CheckPointList[i].GetComponent<Renderer>().material;
+                mat.EnableKeyword("_EMISSION");
+                mat.SetColor("_EmissionColor", FeelData.BlueCheckpointColor * FeelData.ActivatedEmission);
+            }
+        }
+    }
+
+    private bool PlayerInCart(GameObject Player)
+    {
+        Vector3 Offset = Cart.transform.position - Player.transform.position;
+        Offset.y = 0;
+        return Offset.magnitude <= CartRadius;
     }
 
     private void OnPlayerDied(PlayerDied e)
@@ -77,7 +426,7 @@ public class CartModeReforgedArenaManager : MonoBehaviour
             GainExp(Data.KillExp, true);
         }
 
-        RefreshUI();
+        SetUI();
     }
 
     private void GainExp(int Amount, bool Team1)
@@ -94,7 +443,13 @@ public class CartModeReforgedArenaManager : MonoBehaviour
                 int LevelUp = Team1Exp / Data.LevelUpExp;
                 Team1Level += LevelUp;
                 Team1Exp -= LevelUp * Data.LevelUpExp;
+
+                Team1LevelTextHopTimer = 0;
             }
+
+            Team1ExpTextHopTimer = 0;
+            Team1AccumulatedExp += Amount;
+
         }
         else
         {
@@ -108,20 +463,19 @@ public class CartModeReforgedArenaManager : MonoBehaviour
                 int LevelUp = Team2Exp / Data.LevelUpExp;
                 Team2Level += LevelUp;
                 Team2Exp -= LevelUp * Data.LevelUpExp;
+
+                Team2LevelTextHopTimer = 0;
             }
+
+            Team2ExpTextHopTimer = 0;
+            Team2AccumulatedExp += Amount;
         }
+
+
     }
 
     private void OnGameStart(GameStart e)
     {
         GameStart = true;
-    }
-
-    private void RefreshUI()
-    {
-        Team1ExpCounter.GetComponent<Image>().fillAmount = (float)Team1Exp / Data.LevelUpExp;
-        Team1LevelText.GetComponent<TextMeshProUGUI>().text = Team1Level.ToString();
-        Team2ExpCounter.GetComponent<Image>().fillAmount = (float)Team2Exp / Data.LevelUpExp;
-        Team2LevelText.GetComponent<TextMeshProUGUI>().text = Team2Level.ToString();
     }
 }
---
+++ Assets/CartModeReforgedFeelData.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartModeReforgedFeelData : MonoBehaviour
+{
+    public Color NeutralDotlineColor;
+    public Color Team1DotlineColor;
+    public Color Team2DotlineColor;
+
+    public Material EmissiveRed;
+    public Material EmissiveBlue;
+    public Color RedCheckpointColor;
+    public Color BlueCheckpointColor;
+
+    public float ActivatedEmission;
+    public float DeactivatedEmission;
+
+    public float TextHopTime;
+    public float ExpTextStayTime;
+    public float TextDefaultScale;
+    public float TextHopScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

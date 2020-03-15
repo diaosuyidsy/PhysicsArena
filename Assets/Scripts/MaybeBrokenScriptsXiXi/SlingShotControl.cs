734b1a672e12f4a8f35beb30ec732a69f096ffe7
Suyi Diao <suyi.diao@vanderbilt.edu>
2020-03-15 03:42:42 -0400

    Did it w/o remodel, idum

---
+++ Assets/Scripts/MaybeBrokenScriptsXiXi/SlingShotControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlingShotControl : MonoBehaviour
+{
+    public Transform LeftAnchorPoint;
+    public Transform RightAnchorPoint;
+    public Vector2 LeftAnchorPointYClamp;
+    public Vector2 RightAnchorPointYClamp;
+
+    private void LateUpdate()
+    {
+        Vector3 leftRotation = LeftAnchorPoint.eulerAngles;
+        leftRotation.y = Mathf.Clamp(leftRotation.y, LeftAnchorPointYClamp.x, LeftAnchorPointYClamp.y);
+        Vector3 rightRotation = RightAnchorPoint.eulerAngles;
+        leftRotation.y = Mathf.Clamp(rightRotation.y, RightAnchorPointYClamp.x, RightAnchorPointYClamp.y);
+        LeftAnchorPoint.eulerAngles = leftRotation;
+        RightAnchorPoint.eulerAngles = rightRotation;
+    }
+}

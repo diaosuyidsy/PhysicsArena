68f4efd4d8aac45997d06cdce7b02cad53e0eb81
Suyi Diao <suyi.diao@vanderbilt.edu>
2020-03-21 00:25:20 -0400

    Menu Configuration

--- Assets/Scripts/Network/Menu/NetworkMenuPlayerController.cs
+++ Assets/Scripts/Network/Menu/NetworkMenuPlayerController.cs
@@ -1,18 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Mirror;
 
-public class NetworkMenuPlayerController : MonoBehaviour
+public class NetworkMenuPlayerController : NetworkBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

adef5d7ec5abd3c012997ae406ac77edfa1d1cc3
Suyi Diao <suyi.diao@vanderbilt.edu>
2020-03-20 23:57:08 -0400

    Started on Menu

---
+++ Assets/Scripts/Network/Menu/NetworkManagerBirfia.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class NetworkManagerBirfia : NetworkManager
+{
+    public GameObject[] PlayerPrefabs;
+    public GameObject MenuPlayerPrefabs;
+}
